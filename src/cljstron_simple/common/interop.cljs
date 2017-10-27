085bbc22330d52db6174c73de1e2372026693f89
Ivan Pierre <me@ivanpierre.ch>
2017-10-27 10:56:30 +0200

    various stress tests

---
+++ src/cljstron_simple/common/interop.cljs
@@ -0,0 +1,42 @@
+(ns cljstron.common.interop
+  (:require [clojure.string :as s]
+            [cljs.reader :as r]
+            [goog :refer [isNull isObject isFunction]]))
+
+(defn- read-key [key]
+  (let [key (r/read-string key)]
+    (if (symbol? key)
+      (keyword key)
+      (js->clj key))))
+
+(defn- call-func
+  [func args parent]
+  (Reflect.apply func parent (clj->js (into [] args))))
+
+(defn- read-fn
+  [jsobj parent-obj]
+  (if (and parent-obj (.-prototype parent-obj)(.-constructor (.-prototype parent-obj)))
+    #(call-func jsobj %2 %1)
+    #(call-func jsobj % nil)))
+
+(declare ^private rjs->clj)
+
+(defn- key-maker [jsobj pair]
+  (vector (read-key (key pair)) (myjs->clj (val pair) jsobj)))
+
+(defn- read-obj
+  [jsobj parent-obj]
+  (let [obj (js->clj jsobj)]
+    (cond
+      (map? obj)    (into {} (map #(key-maker jsobj %) (seq obj)))
+      (vector? obj) (into [] (map #(myjs->clj % parent-obj) (seq obj))))))
+
+(defn rjs->clj
+  ( [jsobj]
+    (myjs->clj jsobj nil))
+  ( [jsobj parent-obj]
+    (cond
+      (isNull jsobj)   nil
+      (isFunction jsobj) (read-fn jsobj parent-obj)
+      (isObject jsobj)   (read-obj jsobj parent-obj)
+      :else              (js->clj jsobj))))
